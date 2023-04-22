19f267785210738fdf987d9e4bdbcaafbc16c1bd
dom <djrobson2001@gmail.com>
2023-04-22 10:28:54 -0500

    New tab

--- src/components/structure/App.js
+++ src/components/structure/App.js
@@ -6,6 +6,7 @@ import ContactUs from "../ContactUs";
 import Home from "../Home";
 import Services from "../Services";
 import WhyUs from "../WhyUs";
+import PrevWork from '../PrevWork';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
                     <Route path="contact" element={ <ContactUs /> } />
                     <Route path="why-us" element={ <WhyUs /> } />
                     <Route path="services" element={ <Services /> } />
+                    <Route path="prev-work" element={ <PrevWork /> } />
                 </Route>
             </Routes>
         </BrowserRouter>
