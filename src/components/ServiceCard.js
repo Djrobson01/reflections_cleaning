0fe7e27357f651ce1b7fa921cce417f15a91d813
dom <djrobson2001@gmail.com>
2023-04-28 14:54:29 -0500

    updates

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -2,14 +2,16 @@ import { Card } from 'react-bootstrap';
 
 function ServiceCard(props) {
     return (
-        <Card style={{height: 'auto', width: 'auto', margin: 5}}>
+        <Card style={{height: 'auto', width: 'auto', margin: 5}} onClick={() => console.log("Click")}>
             <Card.Img variant="top" src={props.img} alt={`Cover image for ${props.service}`} style={{height: '18rem', width: '60%', justifySelf: 'center', alignSelf: 'center'}}/>
             <Card.Body>
                 <Card.Title>{props.service}</Card.Title>
                 <Card.Text>
                     Add text describing the service
                 </Card.Text>
-                <Card.Link></Card.Link>
+                <Card.Text style={{color: "teal", textAlign: "right"}}>
+                    Click here to request an estimate >
+                </Card.Text>
                 
             </Card.Body>
             
